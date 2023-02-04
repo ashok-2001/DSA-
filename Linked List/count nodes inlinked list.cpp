e63b113697640e1701901062641973bd2939f07e
ASHOK KUMAR <samratashok4656@gmail.com>
2023-02-04 11:44:38 +0530

    gfg practice

---
+++ Linked List/count nodes inlinked list.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+    public:
+    //Function to count nodes of a linked list.
+    int getCount(struct Node* head){
+    
+        //Code here
+        int cnt=0;
+        Node* temp=head;
+        while(temp){
+            cnt++;
+            temp=temp->next;
+        }
+        return cnt;
+    
+    }
+};
+    
